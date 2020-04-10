9f097db200185dcf52a9db90c38715b185b99105
unknown <arinzeokorji@yahoo.com>
2020-04-10 10:11:24 +0100

    Challenge 1 attempt

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,29 @@
-const covid19ImpactEstimator = (data) => data;
+const getInfectionsByRequestedTime = (currentlyInfected, days) => {
+  const power = Math.floor(days / 3);
+  const infectionsByRequestedTime = currentlyInfected * (2 ** power);
+  return infectionsByRequestedTime;
+};
+
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  // Challenge 1
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  impact.infectionsByRequestedTime = getInfectionsByRequestedTime(impact.currentlyInfected, 28);
+
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = getInfectionsByRequestedTime(
+    severeImpact.currentlyInfected, 28
+  );
+
+  return {
+    data: {},
+    impact: {},
+    severeImpact: {}
+  };
+};
+
 
 export default covid19ImpactEstimator;
