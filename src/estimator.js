56cff58cf808b072702bcfe62061e73dc3b8f438
unknown <arinzeokorji@yahoo.com>
2020-04-10 23:54:17 +0100

    Challenge 3 attempt

--- src/estimator.js
+++ src/estimator.js
@@ -115,7 +115,7 @@ const getDollarsInFlight = (infectionsByRequestedTime, averageDailyIncomeInUSD,
   } */
   // console.log('Dollars - ', dollarsInFlight);
 
-  return dollarsInFlight;
+  return dollarsInFlight * 100;
 };
 
 /* const mockData = {
