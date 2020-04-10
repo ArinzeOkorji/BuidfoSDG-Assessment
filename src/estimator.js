b519cdbeec90473409c50ba96075bb1d4ea995b1
unknown <arinzeokorji@yahoo.com>
2020-04-10 14:55:39 +0100

    Challenge 1 attempt

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,14 @@
-const getInfectionsByRequestedTime = (currentlyInfected, days) => {
-  const powerDivision = (days / 3);
+const getInfectionsByRequestedTime = (currentlyInfected, periodType, timeToElapse) => {
+  let numberOfDays;
+  if (periodType === 'days') {
+    numberOfDays = timeToElapse;
+  } else if (periodType === 'weeks') {
+    numberOfDays = timeToElapse * 7;
+  } else if (periodType === 'months') {
+    numberOfDays = timeToElapse * 30;
+  }
+
+  const powerDivision = (numberOfDays / 3);
   let power;
   if (powerDivision.toString().includes('.')) {
     [power] = powerDivision.toString().split('.');
@@ -19,11 +28,13 @@ const covid19ImpactEstimator = (data) => {
   // Challenge 1
 
   impact.currentlyInfected = data.reportedCases * 10;
-  impact.infectionsByRequestedTime = getInfectionsByRequestedTime(impact.currentlyInfected, 28);
+  impact.infectionsByRequestedTime = getInfectionsByRequestedTime(
+    impact.currentlyInfected, data.periodType, data.timeToElapse
+  );
 
   severeImpact.currentlyInfected = data.reportedCases * 50;
   severeImpact.infectionsByRequestedTime = getInfectionsByRequestedTime(
-    severeImpact.currentlyInfected, 28
+    severeImpact.currentlyInfected, data.periodType, data.timeToElapse
   );
 
   return {
