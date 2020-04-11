e779f0d6fe3b650cab103f1c64187d6aa77b487d
unknown <arinzeokorji@yahoo.com>
2020-04-11 22:25:19 +0100

    Frontend challenge attempt

--- src/estimator.js
+++ src/estimator.js
@@ -131,7 +131,7 @@ const getDollarsInFlight = (infectionsByRequestedTime, averageDailyIncomeInUSD,
   totalHospitalBeds: 678874
 }; */
 
-const covid19ImpactEstimator = (data) => {
+exports.covid19ImpactEstimator = (data) => {
   const input = data;
   const impact = {};
   const severeImpact = {};
@@ -195,4 +195,4 @@ const covid19ImpactEstimator = (data) => {
 
 
 // covid19ImpactEstimator(mockData);
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
